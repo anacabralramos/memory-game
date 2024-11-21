b1a392406ae937a99d43499d84260d287a17a8d9
Ana Ramos <anaramos@IOSRTK056.local>
2024-11-21 16:24:40 -0300

    feat: responsive and functional game

--- script.js
+++ script.js
@@ -1,43 +1,62 @@
-const text = "JOGO DA MEMORIA";
-document.getElementById("text").textContent = text;
+// const text = "JOGO DA MEMORIA";
+// document.getElementById("text").textContent = text;
 
-const init = document.getElementById("init");
+const start = document.getElementById("start");
 const greenBtn = document.getElementById("btn-green");
 const redBtn = document.getElementById("btn-red");
 const blueBtn = document.getElementById("btn-blue");
 const yellowBtn = document.getElementById("btn-yellow");
 
+greenBtn.addEventListener("click", async function () {
+  greenBtn.style.pointerEvents = "none";
+  await handlePressButton("GREEN");
+  greenBtn.style.pointerEvents = "auto";
+});
+
+redBtn.addEventListener("click", async function () {
+  redBtn.style.pointerEvents = "none";
+  await handlePressButton("RED");
+  redBtn.style.pointerEvents = "auto";
+});
+
+yellowBtn.addEventListener("click", async function () {
+  yellowBtn.style.pointerEvents = "none";
+  await handlePressButton("YELLOW");
+  yellowBtn.style.pointerEvents = "auto";
+});
+
+blueBtn.addEventListener("click", async function () {
+  blueBtn.style.pointerEvents = "none";
+  await handlePressButton("BLUE");
+  blueBtn.style.pointerEvents = "auto";
+});
+
 var currentStep = 0;
 var currentClick = 0;
 
-const originalSequence = [
-  "GREEN",
-  "RED",
-  "BLUE",
-  "YELLOW",
-  "GREEN",
-  "RED",
-  "BLUE",
-  "YELLOW",
-  "GREEN",
-  "RED",
-  "BLUE",
-  "YELLOW",
-  "GREEN",
-  "RED",
-  "YELLOW",
-];
+const VALUES = ["GREEN", "RED", "BLUE", "YELLOW"];
 
-const handleDisableButtons = (value) => {
-  greenBtn.disabled = value;
-  redBtn.disabled = value;
-  blueBtn.disabled = value;
-  yellowBtn.disabled = value;
+// const originalSequence = ["GREEN", "GREEN", "RED", "RED", "BLUE", "BLUE"];
+
+const originalSequence = new Array(3)
+  .fill()
+  .map(() => VALUES[Math.floor(Math.random() * 4)]);
+
+const toggleButtons = (disabled, pointer) => {
+  greenBtn.disabled = disabled;
+  greenBtn.style.pointerEvents = pointer;
+  redBtn.disabled = disabled;
+  redBtn.style.pointerEvents = pointer;
+  blueBtn.disabled = disabled;
+  blueBtn.style.pointerEvents = pointer;
+  yellowBtn.disabled = disabled;
+  yellowBtn.style.pointerEvents = pointer;
 };
 
-const handlePressButton = (item) => {
+const handlePressButton = async (item) => {
   if (originalSequence[currentClick] === item) {
     currentClick++;
+    await mapColors(item);
   } else {
     return alert("errou");
   }
@@ -45,9 +64,9 @@ const handlePressButton = (item) => {
     currentClick = 0;
     if (++currentStep === originalSequence.length) {
       currentStep = 0;
-      return (init.style.display = "block");
+      return (start.style.display = "block");
     }
-    handleGameTime();
+    handleGameSequence();
   }
 };
 
@@ -61,28 +80,37 @@ const handleTurnButton = (button, style) => {
   });
 };
 
-const handleGameTime = async () => {
-  handleDisableButtons(true);
-  for (var i = 0; i <= currentStep; i++) {
-    switch (originalSequence[i]) {
-      case "GREEN":
-        await handleTurnButton(greenBtn, "btn-green-pressed");
-        break;
-      case "RED":
-        await handleTurnButton(redBtn, "btn-red-pressed");
-        break;
-      case "BLUE":
-        await handleTurnButton(blueBtn, "btn-blue-pressed");
-        break;
-      case "YELLOW":
-        await handleTurnButton(yellowBtn, "btn-yellow-pressed");
-        break;
-    }
+const mapColors = async (color) => {
+  switch (color) {
+    case "GREEN":
+      await handleTurnButton(greenBtn, "btn-green-pressed");
+      break;
+    case "RED":
+      await handleTurnButton(redBtn, "btn-red-pressed");
+      break;
+    case "BLUE":
+      await handleTurnButton(blueBtn, "btn-blue-pressed");
+      break;
+    case "YELLOW":
+      await handleTurnButton(yellowBtn, "btn-yellow-pressed");
+      break;
   }
-  handleDisableButtons(false);
 };
 
+// game sequence
+const handleGameSequence = async () => {
+  toggleButtons(true, "none");
+
+  setTimeout(async () => {
+    for (var i = 0; i <= currentStep; i++) {
+      await mapColors(originalSequence[i]);
+    }
+    toggleButtons(false, "auto");
+  }, 1000);
+};
+
+// game start
 const handleStart = () => {
-  init.style.display = "none";
-  handleGameTime();
+  start.style.display = "none";
+  handleGameSequence();
 };
